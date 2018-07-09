880661d1e48f10d6452515845936e78771c9cc7b
chenkun <462488588@qq.com>
2018-07-09 22:49:23 +0800

    初次提交测试

---
+++ src/com/huawei/TestGit.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.huawei;
+
+/** @author create by urmagical
+*   @date   2018年7月9日 下午10:19:23
+*   @version 
+*/
+public class TestGit {
+	public static void main(String[] args) {
+		System.out.println("abc");
+	}
+}
+
